520db3f35d0625168f09dca7b25223cba65f8b2d
Shivangbijalwan <shivangbijalwan771@gmail.com>
2025-10-09 12:47:10 +0530

    19_switches.js

---
+++ 19_switches.js
@@ -0,0 +1,23 @@
+// these are switch case statements
+const cups = 45;
+switch (cups) {
+    case 4:
+        console.log("value is checkedfor 4");
+        break;
+
+    case 41:
+        console.log("value is checkedfor 41");
+        break;
+    case 43:
+        console.log("value is checkedfor 43");
+        break;
+    case 21:
+        console.log("value is checkedfor 21");
+        break;
+    case 1:
+        console.log("value is checkedfor 1");
+        break;
+    default:
+        console.log("the value of the cup is none of the above")
+        break;
+}
